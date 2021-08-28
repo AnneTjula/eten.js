053df715836b0fe0fe801853d21af215016970c5
AnneTjula <88149819+AnneTjula@users.noreply.github.com>
2021-08-28 11:52:14 +0200

    Add files via upload

---
+++ eten.js
@@ -0,0 +1,25 @@
+
+
+const name = "duka"
+console.log (name)
+
+//problemen met de terminal oplossen met hulp van Duka later
+
+const makePiza = function (){
+    console.log ("make dough and make round bottom");
+    console.log ("cut topping veggies and cheese");
+    console.log ("put toppings on pizza bottom");
+    console.log ("put in oven for 45 minutes");
+};
+makePizza ();
+const makeSushi = function (){
+    console.log ("boil water for rice and add rice");
+    console.log ("no idea what step two is");
+    console.log ("no idea what step three is");
+}
+
+makeSushi ();
+
+makePizza (3);
+makeSushi (2);
+
